5b6dcf45fd4a2bbcb6394ef8a2841261ad639c4b
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-03 16:01:47 +1000

    commit tree b466ea0e3e4fae2ad9957b525997db169deabd4d

---
+++ index.js
@@ -0,0 +1,16 @@
+//Test 1
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+
+//Test 2
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+//Test 3 & 4
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
